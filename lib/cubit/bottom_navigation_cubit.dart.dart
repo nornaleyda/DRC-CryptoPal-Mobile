a980cffb11acc6a7ebc685d37ee7f367312760f7
nornaleyda <naleyda2408rosli@gmail.com>
2023-06-20 21:38:21 +0800

    create cubit for the bottom navigation bar

---
+++ lib/cubit/bottom_navigation_cubit.dart.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import '../views/account.dart';
+import '../views/home.dart';
+import '../views/market.dart';
+
+class BottomNavigationBarCubit extends Cubit<int> {
+  BottomNavigationBarCubit(int initialIndex) : super(initialIndex);
+
+  void updateIndex(int index, BuildContext context) {
+    emit(index);
+    navigateToScreen(index, context);
+  }
+
+  void navigateToScreen(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, anim1, anim2) => const HomePage(),
+            transitionDuration: Duration.zero,
+          ),
+        );
+        break;
+      case 1:
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, anim1, anim2) => const UserAccount(),
+            transitionDuration: Duration.zero,
+          ),
+        );
+        break;
+      case 2:
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, anim1, anim2) => const MarketPage(),
+            transitionDuration: Duration.zero,
+          ),
+        );
+        break;
+      case 3:
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, anim1, anim2) => const UserAccount(),
+            transitionDuration: Duration.zero,
+          ),
+        );
+        break;
+    }
+  }
+}
